690ab9e6fc3499f6bf133279b1c811d3f310c3e9
yuunbou <112618391+yuunbou@users.noreply.github.com>
2023-07-03 13:57:34 +0000

    [5-5]スライドショーステップ１-2

--- slide/slide.js
+++ slide/slide.js
@@ -1,2 +1,19 @@
 'use strict';
 
+const images = ['images/image1.jpg', 'images/image2.jpg', 'images/image3.jpg', 'images/image4.jpg', 'images/image5.jpg'];
+let current = 0;
+
+function changeImage(num) {
+  if(current + num >= 0 && current + num < images.length) {
+    current += num;
+    document.getElementById('main_image').src = images[current];
+  }
+};
+
+document.getElementById('prev').onclick = function() {
+  changeImage(-1);
+};
+document.getElementById('next').onclick = function() {
+  changeImage(1);
+};
+
