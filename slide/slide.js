5aec7134484328c7ab60529eed6479b8629464d4
yuunbou <112618391+yuunbou@users.noreply.github.com>
2023-07-06 13:34:06 +0000

    [5-5]スライドショーステップ2

--- slide/slide.js
+++ slide/slide.js
@@ -7,8 +7,15 @@ function changeImage(num) {
   if(current + num >= 0 && current + num < images.length) {
     current += num;
     document.getElementById('main_image').src = images[current];
+    pageNum();
   }
 };
+
+function pageNum() {
+  document.getElementById('page').textContent = `${current + 1}/${images.length}`;
+}
+
+pageNum();
 
 document.getElementById('prev').onclick = function() {
   changeImage(-1);
